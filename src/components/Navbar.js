059fbea078d79cfb7127902495421c1e92f3b273
Maddie Thomas <7356701+madelynthomas@users.noreply.github.com>
2020-08-23 17:35:47 -0700

    Replaced all instances of class with JSX friendly className

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,9 +3,9 @@ import React from "react";
 export default function Navbar() {
   return (
     <React.Fragment>
-      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <button
-          class="navbar-toggler"
+          className="navbar-toggler"
           type="button"
           data-toggle="collapse"
           data-target="#navbar"
@@ -13,31 +13,31 @@ export default function Navbar() {
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
-          <span class="navbar-toggler-icon"></span>
+          <span className="navbar-toggler-icon"></span>
         </button>
 
         <div
-          class="collapse navbar-collapse justify-content-md-center"
+          className="collapse navbar-collapse justify-content-md-center"
           id="navbar"
         >
-          <ul class="navbar-nav">
-            <li class="nav-item active">
-              <a class="nav-link" href="#:">
-                State Parks <span class="sr-only">(current)</span>
+          <ul className="navbar-nav">
+            <li className="nav-item active">
+              <a className="nav-link" href="#:">
+                State Parks <span className="sr-only">(current)</span>
               </a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#:">
+            <li className="nav-item">
+              <a className="nav-link" href="#:">
                 Home
               </a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#:">
+            <li className="nav-item">
+              <a className="nav-link" href="#:">
                 About
               </a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#:">
+            <li className="nav-item">
+              <a className="nav-link" href="#:">
                 Landmarks
               </a>
             </li>
